4389afc2326bb23f04116583d9d7b8d92a57c2ff
Jhosimar Aguacia <corbin026@hotmail.com>
2023-01-31 22:10:32 +0200

    Learn about the math object

---
+++ js_sandbox/math.js
@@ -0,0 +1,53 @@
+const num1 = 500;
+const num2 = 50;
+let result;
+
+// Addition
+result = num1 + num2;
+console.log('Addition: ', result);
+// Subtraction
+result = num1 - num2;
+console.log('Subtraction: ', result);
+// Multiplication
+result = num1 * num2;
+console.log('Multiplication: ', result);
+// Division
+result = num1 / num2;
+console.log('Division: ', result);
+// Modulus
+result = num1 % num2;
+console.log('Modulus: ', result);
+
+// Math Object
+result = Math.PI;
+console.log('PI: ', result);
+// Round
+result = Math.round(2.4);
+console.log('Round: ', result);
+// Round up
+result = Math.ceil(2.4);
+console.log('Round up: ', result);
+// Round down
+result = Math.floor(2.8);
+console.log('Round down: ', result);
+// Square root
+result = Math.sqrt(64);
+console.log('Square root: ', result);
+// Absolute value
+result = Math.abs(-3);
+console.log('Absolute value: ', result);
+// Power
+result = Math.pow(8, 2);
+console.log('Power: ', result);
+// Minimum
+result = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+console.log('Minimum: ', result);
+// Maximum
+result = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+console.log('Maximum: ', result);
+// Random number
+result = Math.random();
+console.log('Random number: ', result);
+// Random number between 1 and 20
+result = Math.floor(Math.random() * 20 + 1);
+console.log('Random number between 1 and 20: ', result);
